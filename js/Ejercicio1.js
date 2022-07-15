328688be6466eb1495b22a69052824f12d79fdbb
rasalgh0ul <arkantos983@gmail.com>
2022-07-14 21:13:47 -0300

    reiniciando proyecto

---
+++ js/Ejercicio1.js
@@ -0,0 +1,10 @@
+let edad = parseInt(prompt("Introduce tu edad"));
+
+
+if(edad >= 18){
+    document.write("Felicidades puedes conducir!!!!")
+}else if(edad < 18){
+    document.write("No eres mayor de 18 años, no puedes conducir")
+}else{
+    document.write("Introduciste un dato no valido")
+} 
